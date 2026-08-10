12d6459f9674daf26de4d789cd61c7bdd7fbdefb
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2026-08-10 14:57:48 +0530

    Time: 3 ms (75.61%) | Memory: 134.3 MB (63.44%) - LeetSync

--- 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -4,8 +4,9 @@ public:
         int n = nums.size();
         vector<int> ans(n,-1);
         int window = 2*k+1;
-        if (window>n)
+        if(window>n){
             return ans;
+        }
         long long sum=0;
         for (int i=0;i<window;i++) {
             sum+=nums[i];
