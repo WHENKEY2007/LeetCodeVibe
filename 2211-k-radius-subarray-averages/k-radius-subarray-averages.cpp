131f7d642230c1214d45b9cd0b679cc289a5d64e
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2026-08-10 14:38:57 +0530

    Time: 7 ms (42.97%) | Memory: 134.3 MB (82.78%) - LeetSync

---
+++ 2211-k-radius-subarray-averages/k-radius-subarray-averages.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> getAverages(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n,-1);
+        int window = 2*k+1;
+        if (window>n)
+            return ans;
+        long long sum=0;
+        for (int i=0;i<window;i++) {
+            sum+=nums[i];
+        }
+        ans[k]=sum/window;
+        for (int r=window;r<n;r++) {
+            sum+=nums[r];
+            sum-=nums[r-window];
+            int center=r-k;
+            ans[center]=sum/window;
+        }
+        return ans;
+    }
+};
