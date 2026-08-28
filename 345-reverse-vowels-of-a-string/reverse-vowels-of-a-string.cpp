53c0253ec03a60ebf76adfff4ada161319f52f01
N.Sri Venkatesh <srivenkateshnurukurthi@gmail.com>
2026-08-28 15:10:20 +0530

    Time: 3 ms (32.75%) | Memory: 10.3 MB (47.42%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    string reverseVowels(string s) {
-        int len=s.length();
-        int i=0,j=len-1;
-            while(i<j){
-    if(!(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'||
-        s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U')){
-        i++;
-    }  
-    else if(!(s[j]=='a'||s[j]=='e'||s[j]=='i'||s[j]=='o'||s[j]=='u'||
-            s[j]=='A'||s[j]=='E'||s[j]=='I'||s[j]=='O'||s[j]=='U')){
-        j--;
+    string reverseVowels(string s) 
+    {
+        int j=s.size()-1;
+        int i=0;
+        while(i<j)
+        {
+            if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') && (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'))
+            {
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+            else
+            {
+                if ((s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') && !(s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u' || s[j] == 'A' || s[j] == 'E' || s[j] == 'I' || s[j] == 'O' || s[j] == 'U'))
+                {
+                        j--;
+                }
+                else
+                    i++;
+            }
+        } 
+        return s;   
     }
-    else{
-        char temp=s[i];
-        s[i]=s[j];
-        s[j]=temp;
-        i++;
-        j--;
-    }   
-
-}
- return s;
-    }
-   
 };
